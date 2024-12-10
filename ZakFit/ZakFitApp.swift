dd55f1aa38da0d6d77ec64d85c6e7d027b714423
Audrey <ahda.audrey92@gmail.com>
2024-12-10 14:22:19 +0100

    Initial Commit

---
+++ ZakFit/ZakFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFitApp.swift
+//  ZakFit
+//
+//  Created by Audrey on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZakFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZakFitTests/ZakFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFitTests.swift
+//  ZakFitTests
+//
+//  Created by Audrey on 10/12/2024.
+//
+
+import Testing
+@testable import ZakFit
+
+struct ZakFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
